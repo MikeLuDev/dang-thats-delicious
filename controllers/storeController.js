3b707fe6a43accea5eabdf44c86283b792527671
Michael Lu <michaelwlu@live.com>
2018-05-19 19:12:58 -0400

    Added method to update database with new store information

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,13 +1,23 @@
+// Import mongoose
+const mongoose = require('mongoose');
+
+// We're able to import the Store model here because it has been imported already in start.js
+// And Mongoose is a singleton
+const Store = mongoose.model('Store');
+
+// Render home page
 exports.homePage = (req, res) => {
-  console.log(req.name);
   res.render('index')
 }
 
+// Render edit store page
 exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 }
 
-exports.createStore = (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+// Save new store to DB
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  res.redirect('/');
 }
