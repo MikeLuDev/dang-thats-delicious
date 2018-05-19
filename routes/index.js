9a9f40c0bab698d22191361807caeb5fa9b76337
Michael Lu <michaelwlu@live.com>
2018-05-19 19:13:10 -0400

    Added error handling

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
+// Import higher order catch error function to wrap our routes in
+const { catchErrors } = require('../handlers/errorHandlers');
+
+// Routing
 router.get('/', storeController.homePage);
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
