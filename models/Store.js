481fee77af427dc284168e26a817119f91a3b680
Michael Lu <michaelwlu@live.com>
2018-05-22 12:11:49 -0400

    Formatting, added fields for location and creation date

--- models/Store.js
+++ models/Store.js
@@ -2,21 +2,42 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
+// Schema for handling Store table
 const storeSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: 'Please enter a store name'
+    required: 'Please enter a store name',
   },
   slug: String,
   description: {
     type: String,
-    trim: true
+    trim: true,
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point',
+    },
+    coordinates: [
+      {
+        type: Number,
+        required: 'You must supply coordinates.',
+      },
+    ],
+    address: {
+      type: String,
+      required: 'You must supply an address.',
+    },
+  },
 });
 
-storeSchema.pre('save', function (next) {
+storeSchema.pre('save', function(next) {
   if (!this.isModified('name')) {
     next(); // skip it
     return; // stop function from running
