e1d662bd3ff453ed005c71b2b19a74d8f87717f8
Michael Lu <michaelwlu@live.com>
2018-05-28 15:42:11 -0400

    Add register middleware

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
+// Render login form
 exports.loginForm = (req, res) => {
   res.render('login', { title: 'Login' });
-}
+};
 
+// Render register form
 exports.registerForm = (req, res) => {
   res.render('register', { title: 'Register' });
-}
+};
 
+// Validate user registration
 exports.validateRegister = (req, res, next) => {
   req.sanitizeBody('name');
   req.checkBody('name', 'You must supply a name').notEmpty();
@@ -26,5 +31,13 @@ exports.validateRegister = (req, res, next) => {
     res.render('register', { title: 'Register', body: req.body, flashes: req.flash() });
     return;
   }
+  next();
+};
+
+// Save new user to db
+exports.register = async (req, res, next) => {
+  const user = await new User({ email: req.body.email, name: req.body.name });
+  const register = promisify(User.register, User);
+  await register(user, req.body.password);
   next();
 };
